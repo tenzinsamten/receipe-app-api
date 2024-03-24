ebc9d5d49b13b7160a9969404b01c7d04902997c
tenzinsamten <iamsamten>
2024-03-24 18:15:14 +0530

    unit test init

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+from django.test import SimpleTestCase
+
+from app import calculator
+
+class CalculatorTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calculator.add(5,6)
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        res = calculator.subtract(10,15)
+        self.assertEqual(res,-5)
+
+    def test_subtract_numbers_in_postive(self):
+        res = calculator.subtract(10,6)
+        self.assertEqual(res,4)
+    
+    def test_subtract_numbers_in_0(self):
+        res = calculator.subtract(0,6)
+        self.assertEqual(res,4)    
