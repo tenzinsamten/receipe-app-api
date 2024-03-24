f5bf61139024b0d9e6f5213b397e9e6c02ff6379
tenzinsamten <iamsamten>
2024-03-24 20:19:37 +0530

    chore: resolved the lint issues

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,15 +1,16 @@
 """
 Django command to wait for the database to be available
 """
-from django.core.management.base import BaseCommand
 from django.db.utils import OperationalError
 from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management.base import BaseCommand
 import time
 
+
 class Command(BaseCommand):
     """ Django command to wait for database."""
-    def handle(self,*args,**options):
+
+    def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
@@ -18,8 +19,8 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
 
-            except(Psycopg2Error,OperationalError):
-                self.stdout.write('Dababase is not ready yet,waiting for 1 second..')
+            except(Psycopg2Error, OperationalError):
+                self.stdout.write('Database is not ready yet')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available'))        
+        self.stdout.write(self.style.SUCCESS('Database available'))
