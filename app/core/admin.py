706393113614ebf46648c6b2bc4b529acd1f1daf
tenzinsamten <iamsamten>
2024-03-25 21:42:17 +0530

    admin user listing users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """define the admin page for users"""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.User, UserAdmin)
